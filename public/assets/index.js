ee5fcbd8d3e546e4e23a345802f875bc8a117dbb
Andrew Garcia <andrewagarcia8@gmail.com>
2022-03-17 17:34:20 -0700

    Inital Commitl

--- public/assets/index.js
+++ public/assets/index.js
@@ -77,9 +77,9 @@ const handleNoteSave = () => {
   });
 };
 
-// Delete the clicked note
+
 const handleNoteDelete = (e) => {
-  // Prevents the click listener for the list from being called when the button inside of it is clicked
+  
   e.stopPropagation();
 
   const note = e.target;
